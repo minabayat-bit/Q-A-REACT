5adbfbb16235ed1ecbe381753f08fe6b24e5f617
minabayat <minabayatt9@gmail.com>
2025-10-26 19:54:12 +0330

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Cards from "./components/Cards";
 import "./index.css";
 
-function App() {
+function App()  {
+
+const [openIdex, setOpenIdex] = useState<number | null : index>(null);
+const toggleCard = (index: number) => {
+  setOpenIdex(openIdex === index ? null : index);
+};
+
   return (
-    <div className="flex flex-col justify-center items-center pt-50 gap-4 ">
+    <div className=" flex flex-col justify-center items-center pt-50 gap-4 ">
       <span className="text-[#c59d5f] text-4xl font-light tracking-wider">
         General Questions
       </span>
-      <div className="bg-white p-8 gap-3">
-        <div className="flex flex-row justify-between">
-          <h2 className="tracking-widest">
+      <Cards >
+        <div >
+          <h2 >
             do you accept all major credit cards?
           </h2>
-          <button className="text-[#c59d5f] text-[25px] border-2 border-[#c59d5f] ">+</button>
+          <button >
+            +
+          </button>
         </div>
-        <div className="h-1 bg-black"></div>
-        <p className="text-gray-400 w-150 pt-3">
+        <div ></div>
+        <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolore
           illo dolores quia nemo doloribus quaerat, magni numquam repellat
           reprehenderit.
         </p>
-      </div>
+      </Cards>
     </div>
   );
 }
---
+++ src/components/Cards.tsx
@@ -0,0 +1,31 @@
+import React, { Children } from 'react';
+import { type ReactNode } from 'react';
+
+interface cardsPropsType {
+    Children: React.ReactNode;
+}
+
+const Cards = ({ children }: cardsPropsType) => {
+  return (
+    <div>
+        <div className="bg-white p-8 gap-3">
+        <div className="flex flex-row justify-between">
+          <h2 className="tracking-widest">
+            
+          </h2>
+          <button onClick={ontoggle} className="text-[#c59d5f] text-[25px] border-2 border-[#c59d5f] ">
+            +
+          </button>
+        </div>
+        <div className="h-1 bg-black"></div>
+        <p className="text-gray-400 w-150 pt-3">
+          
+        </p>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default Cards
+
