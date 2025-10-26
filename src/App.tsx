fd95096522727bb52a580cea383f2516255d7530
minabayat <minabayatt9@gmail.com>
2025-10-26 20:47:33 +0330

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,31 @@ import "./App.css";
 import Cards from "./components/Cards";
 import "./index.css";
 
-function App()  {
-
-const [openIndex, setOpenIndex] = useState<number | null>(null);
-const toggleCard = (index: number) => {
-  setOpenIndex(openIndex === index ? null : index);
-};
+function App() {
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+  const toggleCard = (index: number) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
 
   return (
     <div className=" flex flex-col justify-center items-center pt-50 gap-4 ">
       <span className="text-[#c59d5f] text-4xl font-light tracking-wider">
         General Questions
       </span>
-      <Cards >
-        <div >
-          <h2 >
-            do you accept all major credit cards?
-          </h2>
-          <button 
-          onClick={() => toggleCard(0)} className="text-[#c59d5f] text-[25px] border-2 border-[#c59d5f] " >
+      <Cards>
+        <div className="flex flex-row justify-between">
+          <h2>do you accept all major credit cards?</h2>
+          <button onClick={() => toggleCard(0)}>
             {openIndex === 0 ? "-" : "+"}
           </button>
         </div>
-        <div ></div>
+        <div></div>
         {openIndex === 0 && (
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolore
-          illo dolores quia nemo doloribus quaerat, magni numquam repellat
-          reprehenderit.
-        </p>
+          <p className="">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolore
+            illo dolores quia nemo doloribus quaerat, magni numquam repellat
+            reprehenderit.
+          </p>
         )}
       </Cards>
     </div>
--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -6,15 +6,21 @@ interface cardsPropsType {
 
 const Cards = ({ children }: cardsPropsType) => {
   return (
-    <div className=" flex flex-col justify-center items-center pt-50 gap-4 ">
+    <div className=" flex flex-col justify-center items-center  gap-4 ">
       <div className="bg-white p-8 gap-3">
         <div className="flex flex-row justify-between">
           <h2 className="tracking-widest"></h2>
+          <button 
+          className="text-[#c59d5f] text-[25px] border-2 border-[#c59d5f] " >
+            
+          </button>
+
         </div>
         <div className="h-px bg-black"></div>
         <p className="text-gray-400 w-100 pt-3"></p>
+        {children}
       </div>
-      {children}
+      
     </div>
   );
 };
