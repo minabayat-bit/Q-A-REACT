0c2b33f7a7c1f5c995016d73968cfd786d0801c8
minabayat <minabayatt9@gmail.com>
2025-10-26 23:39:21 +0330

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,20 @@ import Cards from "./components/Cards";
 import "./index.css";
 
 function App() {
-  const [openIndex, setOpenIndex] = useState<number | null>(null);
-  const toggleCard = (index: number) => {
-    setOpenIndex(openIndex === index ? null : index);
-  };
+  // const [openIndex, setOpenIndex] = useState<number | null>(null);
+  // const toggleCard = (index: number) => {
+  //   setOpenIndex(openIndex === index ? null : index);
+  // };
 
   return (
     <div className=" flex flex-col justify-center items-center pt-50 gap-4 ">
       <span className="text-[#c59d5f] text-4xl font-light tracking-wider">
         General Questions
       </span>
-      <Cards>
-        <div className="flex flex-row justify-between">
-          <h2>do you accept all major credit cards?</h2>
-          <button onClick={() => toggleCard(0)}>
-            {openIndex === 0 ? "-" : "+"}
-          </button>
-        </div>
-        <div></div>
-        {openIndex === 0 && (
-          <p className="">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolore
-            illo dolores quia nemo doloribus quaerat, magni numquam repellat
-            reprehenderit.
-          </p>
-        )}
-      </Cards>
+
+      <Cards/>
+      <Cards />
+      <Cards />
     </div>
   );
 }
--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,26 +1,37 @@
-import { type ReactNode } from "react";
+import { useState } from "react";
 
-interface cardsPropsType {
-  children: ReactNode;
-}
-
-const Cards = ({ children }: cardsPropsType) => {
+const Cards = () => {
+  const [openIndex, setOpenIndex] = useState(false);
   return (
     <div className=" flex flex-col justify-center items-center  gap-4 ">
-      <div className="bg-white p-8 gap-3">
+      <div className="bg-white p-8 gap-3 w-150">
         <div className="flex flex-row justify-between">
-          <h2 className="tracking-widest"></h2>
-          <button 
-          className="text-[#c59d5f] text-[25px] border-2 border-[#c59d5f] " >
-            
+          <h2 className="tracking-widest">title card</h2>
+          <button
+            onClick={() => {
+              if (openIndex === true) {
+                setOpenIndex(false);
+              } else {
+                setOpenIndex(true);
+              }
+            }}
+            className="text-[#c59d5f] text-[25px] font-bold border-3 border-[#c59d5f]  w-6 h-10  "
+          >
+            {" "}
+            {openIndex ? "-" : "+"}
           </button>
-
         </div>
-        <div className="h-px bg-black"></div>
-        <p className="text-gray-400 w-100 pt-3"></p>
-        {children}
+        <div className="h-px bg-black mt-4"></div>
+        {openIndex ? (
+
+        <p className="text-gray-400 w-100 pt-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+          iusto quae expedita ducimus blanditiis, qui repudiandae officiis
+          numquam fugiat at porro aut vel quis cumque aliquid odio, harum
+          provident odit.
+        </p>
+        ) : null}
       </div>
-      
     </div>
   );
 };
